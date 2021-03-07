7c3031bbc7e5b1c445a990c2030f3600cff4142c
Tim Douglas <neurotech@gmail.com>
2021-03-07 21:43:46 +1100

    Simpler export

---
+++ src/definitions.ts
@@ -0,0 +1,35 @@
+export type BoxProps = {
+  children?: React.ReactNode;
+  backgroundColor?: string;
+  flexGrow: number;
+  flexShrink: number;
+  space: Space;
+  padding?: number;
+};
+
+export enum JustifyContent {
+  Center = "center",
+  End = "end",
+  FlexEnd = "flex-end",
+  FlexStart = "flex-start",
+  Inherit = "inherit",
+  Initial = "initial",
+  Left = "left",
+  Normal = "normal",
+  Right = "right",
+  SpaceAround = "space-around",
+  SpaceBetween = "space-between",
+  SpaceEvenly = "space-evenly",
+  Start = "start",
+  Stretch = "stretch",
+  Unset = "unset",
+}
+
+export enum Space {
+  None = "0",
+  XXSmall = "4px",
+  XSmall = "8px",
+  Small = "16px",
+  Medium = "32px",
+  Large = "64px",
+}
