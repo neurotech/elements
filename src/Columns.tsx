09cc0916b4c92b3d2bc2b690210d0f2d1e9c1c05
Tim Douglas <neurotech@gmail.com>
2021-03-10 20:40:42 +1100

    Added Columns component

---
+++ src/Columns.tsx
@@ -0,0 +1,60 @@
+import React, { createContext } from "react";
+import { JustifyContent, Space } from "./definitions";
+import styled from "styled-components";
+
+type ColumnsProps = {
+  alignItems?: string;
+  children?: React.ReactNode;
+  flexWrap?: string;
+  flow?: string;
+  justifyContent: JustifyContent;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+  reverse: boolean;
+  space: Space;
+};
+
+const resolveReverseToValue = (prefix: string, reverse: boolean) => {
+  return reverse ? `${prefix}-reverse` : prefix;
+};
+
+const StyledColumns = styled.div<ColumnsProps>`
+  display: flex;
+  flex-direction: ${(props) => resolveReverseToValue("row", props.reverse)};
+  flex-flow: ${(props) => props.flow};
+  justify-content: ${(props) =>
+    props.justifyContent ? props.justifyContent : "flex-start"};
+  align-items: ${(props) => props.alignItems};
+  margin-left: -${(props) => props.space};
+  flex-wrap: ${(props) => props.flexWrap};
+  overflow-wrap: break-word;
+  cursor: ${(props) => (props.onClick ? "pointer" : "default")};
+`;
+
+export const ColumnsContext = createContext({
+  space: Space.Small,
+});
+
+export const Columns = (props: ColumnsProps) => {
+  return (
+    <StyledColumns
+      alignItems={props.alignItems}
+      flexWrap={props.flexWrap}
+      flow={props.flow}
+      justifyContent={props.justifyContent}
+      onClick={props.onClick}
+      reverse={props.reverse}
+      space={props.space}
+    >
+      <ColumnsContext.Provider value={{ space: props.space }}>
+        {props.children}
+      </ColumnsContext.Provider>
+    </StyledColumns>
+  );
+};
+
+Columns.defaultProps = {
+  flexWrap: "wrap",
+  justifyContent: JustifyContent.Unset,
+  reverse: false,
+  space: Space.Small,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./definitions";
 export * from "./Box";
+export * from "./Columns";
 export * from "./Stack";
