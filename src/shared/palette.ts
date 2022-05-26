5aef7e6b53ad845cf0abc9de6023861dbf6ed062
Tim Douglas <neurotech@gmail.com>
2022-05-26 20:13:39 +1000

    Added palette

---
+++ src/shared/palette.ts
@@ -0,0 +1,30 @@
+export const palette = {
+  black: "#212328",
+  white: "#ffffff",
+
+  darkgreen: "#005745",
+  green: "#00b894",
+  lightgreen: "#6cffe2",
+
+  darkgrey: "#404040",
+  grey: "#919191",
+  lightgrey: "#d4d4d4",
+
+  darkash: "#212328",
+  ash: "#4d4e5f",
+  lightash: "#85869b",
+
+  darkred: "#460012",
+  lightred: "#ff92ad",
+  glassred: "rgba(255, 56, 106, 0.25)",
+  red: "#ff386a",
+
+  blue: "#5077f3",
+  darkblue: "#0a1c4c",
+  glassblue: "rgba(80, 118, 243, 0.25)",
+  lightblue: "#6b8bf5",
+
+  darkyellow: "#68460b",
+  lightyellow: "#ffc561",
+  yellow: "#ffb83c",
+};
