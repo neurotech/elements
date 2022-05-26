e7e41a4efc3b30f2ac174e500eb6d22393a921d7
Tim Douglas <neurotech@gmail.com>
2022-05-26 20:14:08 +1000

    Added dark and light theme

---
+++ src/shared/theme.ts
@@ -0,0 +1,119 @@
+import { palette } from "./palette";
+
+export type ThemeType = "light" | "dark";
+
+const globalStyles = {
+  fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"`,
+  minHeight: "2.5rem",
+  selection: {
+    background: palette.blue,
+    text: palette.white,
+  },
+};
+
+const buttonStyles = {
+  button: {
+    disabled: {
+      background: palette.ash,
+      border: palette.darkash,
+      text: palette.darkash,
+    },
+    blue: {
+      background: palette.blue,
+      border: palette.darkblue,
+      text: palette.white,
+      textShadow: palette.darkblue,
+      hover: {
+        background: palette.lightblue,
+        text: palette.darkblue,
+      },
+    },
+    green: {
+      background: palette.green,
+      border: palette.darkgreen,
+      text: palette.white,
+      textShadow: palette.darkgreen,
+      hover: {
+        background: palette.lightgreen,
+        text: palette.darkgreen,
+      },
+    },
+    grey: {
+      background: palette.grey,
+      border: palette.darkgrey,
+      text: palette.white,
+      textShadow: palette.darkgrey,
+      hover: {
+        background: palette.lightgrey,
+        text: palette.darkgrey,
+      },
+    },
+    red: {
+      background: palette.red,
+      border: palette.darkred,
+      text: palette.white,
+      textShadow: palette.darkred,
+      hover: {
+        background: palette.lightred,
+        text: palette.darkred,
+      },
+    },
+    yellow: {
+      background: palette.yellow,
+      border: palette.darkyellow,
+      text: palette.white,
+      textShadow: palette.darkyellow,
+      hover: {
+        background: palette.lightyellow,
+        text: palette.darkyellow,
+      },
+    },
+  },
+  input: {
+    background: palette.darkgrey,
+    border: palette.grey,
+    text: palette.lightgrey,
+    placeholder: palette.grey,
+    disabled: {
+      background: palette.darkash,
+      border: palette.ash,
+      text: palette.lightash,
+    },
+    focused: {
+      background: palette.glassblue,
+      border: palette.lightblue,
+      shadow: palette.glassblue,
+      placeholder: palette.lightblue,
+      invalid: {
+        background: palette.glassred,
+        border: palette.lightred,
+        shadow: palette.glassred,
+        placeholder: palette.lightred,
+      },
+    },
+    invalid: {
+      background: palette.darkgrey,
+      border: palette.red,
+      text: palette.red,
+    },
+    label: {
+      text: palette.white,
+      invalid: {
+        text: palette.red,
+      },
+    },
+  },
+};
+
+export const themes = {
+  light: {
+    ...globalStyles,
+    ...buttonStyles,
+    background: palette.white,
+  },
+  dark: {
+    ...globalStyles,
+    ...buttonStyles,
+    background: palette.black,
+  },
+};
